15f1bf6c2a5fcfd57251a9cba5b93927634b791e
Seongha Park <paseongha@yahoo.co.uk>
2020-09-25 09:58:47 +0100

    Add project files.

---
+++ Scramble/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Scramble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string word = Console.ReadLine();
+                string capitalisedword = word.ToUpper();
+                char[] wordArray = capitalisedword.ToCharArray();
+                Random rnd = new Random();
+                char[] MyRandomArray = wordArray.OrderBy(x => rnd.Next()).ToArray();
+                Console.WriteLine(MyRandomArray);
+            }
+        }
+    }
+}
